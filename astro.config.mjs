3cc0c28393f140d56abab750a2902eccdca4feea
Elton Bahia <eltonbahiabc@gmail.com>
2026-05-17 19:10:52 -0300

    Use manaos.cloud as canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.manaoscloud.com',
+  site: 'https://manaos.cloud',
   integrations: [sitemap()],
 });
